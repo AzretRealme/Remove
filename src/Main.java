e1b951eee75769fae2a554c04d781c0424baa55c
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-01-12 09:47:41 +0600

    updated
    updated

--- src/Main.java
+++ src/Main.java
@@ -21,15 +21,27 @@ public class Main {
                 System.out.println(s.name + " " + s.phone);
             }
         }
+        Student student = new Student("Aibek", "9965554444");
+        Student student1 = new Student("Karls", "99655322222");
+        easyIt.studentCount(2);
+        boolean g1 = easyIt.addStudents(student);
+        boolean g2 = easyIt.addStudents(student1);
+        System.out.println(g1 + " " + g2);
+
+        for (Student h : easyIt.students) {
+            if (h != null) {
+                System.out.println(h.name + " " + h.phone);
+            }
+        }
 
         Student[] students = new Student[7];
-        students[0] = new Student("Aibek", "996555444444 ");
+        students[0] = new Student("Aibek", "996555444444");
         students[1] = new Student("Alice", "996555333333");
         students[2] = new Student("Aijan", "996555222222");
-        students[3] = new Student("Talgat", " 996555777777");
-        students[4] = new Student("Aliya", " 996555888888");
-        students[5] = new Student("Bermet", " 996555111111");
-        students[6] = new Student("Aidana", " 996555121212");
+        students[3] = new Student("Talgat", "996555777777");
+        students[4] = new Student("Aliya", "996555888888");
+        students[5] = new Student("Bermet", "996555111111");
+        students[6] = new Student("Aidana", "996555121212");
 
         Course newStudents = new Course();
         newStudents.name = "newStudents";
@@ -40,24 +52,28 @@ public class Main {
         System.out.println( students[5].name + " " + students[5].phone + " " + students[6].name + " " + students[6].phone);
 
 
-        newStudents.studentCount(5);
+        newStudents.studentCount(7);
         newStudents.addStudents(students[0]);
         newStudents.addStudents(students[1]);
         newStudents.addStudents(students[2]);
         newStudents.addStudents(students[3]);
         newStudents.addStudents(students[4]);
-        students[5] = null;
-        students[6] = null;
+        newStudents.addStudents(students[5]);
+        newStudents.addStudents(students[6]);
 
-        while(newStudents.students.length > 0){
-            System.out.print(" Enter students phone number to remove: ");
+        while(newStudents.students.length > 0) {
+                System.out.print(" Enter 1 students phone number to remove: ");
+                String phoneNumber = scan.nextLine();
+                newStudents.removerStudent(phoneNumber);
+                System.out.println(newStudents.manyStudents);
 
-            String phoneNumber = scan.nextLine();
-            newStudents.removerStudent(phoneNumber);
 
-            System.out.println(newStudents.manyStudents + "students left" + " Bermet and Aidana");
-            break;
-        }
+                System.out.print(" Enter 2 students phone number to remove: ");
+                String phoneNumber2 = scan.nextLine();
+                newStudents.removerStudent2(phoneNumber2);
+                System.out.println(newStudents.manyStudents);
+                break;
+            }
 
 
         for (Student d : newStudents.students) {
